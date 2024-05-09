cfa2c3d1c1970db9105dae6bb2483f3562c85ce9
Raphael_Netoo <rafaelneto3047@gmail.com>
2024-05-09 14:54:50 -0300

    Deu certo, finalmente kk

--- scripts/modelo_restaurante.js
+++ scripts/modelo_restaurante.js
@@ -1,10 +1,35 @@
 function abrirVerMais() {
     const modal = document.querySelector('.janela-modal')
+
+    const sobre = document.querySelector('.sobre')
+    const horario = document.querySelector('.horario')
+    const pagamento = document.querySelector('.pagamento')
+
     modal.classList.add('abrir')
+
+    sobre.classList.add('abrir')
+    horario.classList.remove('abrir')
+    pagamento.classList.remove('abrir')
 
     modal.addEventListener('click', (e) => {
         if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
             modal.classList.remove('abrir')
+        }
+
+        if (e.target.id == 'sobre') {
+            sobre.classList.add('abrir')
+            horario.classList.remove('abrir')
+            pagamento.classList.remove('abrir')
+        }
+        if (e.target.id == 'horario') {
+            sobre.classList.remove('abrir')
+            horario.classList.add('abrir')
+            pagamento.classList.remove('abrir')
+        }
+        if (e.target.id == 'pagamento') {
+            sobre.classList.remove('abrir')
+            horario.classList.remove('abrir')
+            pagamento.classList.add('abrir')
         }
     })
 }
