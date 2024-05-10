a65f55b2b4ae62ddd111f8ebe3da5e00fcf77f03
Raphael_Netoo <rafaelneto3047@gmail.com>
2024-05-09 23:25:03 -0300

    modal-opcaoDeEntrega funfando agora

--- scripts/modelo_restaurante.js
+++ scripts/modelo_restaurante.js
@@ -1,35 +1,69 @@
 function abrirVerMais() {
     const modal = document.querySelector('.janela-modal')
 
-    const sobre = document.querySelector('.sobre')
-    const horario = document.querySelector('.horario')
-    const pagamento = document.querySelector('.pagamento')
+    const divSobre = document.querySelector('.sobre')
+    const divHorario = document.querySelector('.horario')
+    const divPagamento = document.querySelector('.pagamento')
+
+    const opcaoSobre = document.querySelector('#sobre')
+    const opcaoHorario = document.querySelector('#horario')
+    const opcaoPagamento = document.querySelector('#pagamento')
 
     modal.classList.add('abrir')
 
-    sobre.classList.add('abrir')
-    horario.classList.remove('abrir')
-    pagamento.classList.remove('abrir')
+    divSobre.classList.add('abrir')
+    divHorario.classList.remove('abrir')
+    divPagamento.classList.remove('abrir')
+
+    opcaoSobre.classList.add('indicacao-modal-vermais')
 
     modal.addEventListener('click', (e) => {
         if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
             modal.classList.remove('abrir')
+
+            opcaoSobre.classList.remove('indicacao-modal-vermais')
+            opcaoHorario.classList.remove('indicacao-modal-vermais')
+            opcaoPagamento.classList.remove('indicacao-modal-vermais')
         }
 
         if (e.target.id == 'sobre') {
-            sobre.classList.add('abrir')
-            horario.classList.remove('abrir')
-            pagamento.classList.remove('abrir')
+            divSobre.classList.add('abrir')
+            divHorario.classList.remove('abrir')
+            divPagamento.classList.remove('abrir')
+
+            opcaoSobre.classList.add('indicacao-modal-vermais')
+            opcaoHorario.classList.remove('indicacao-modal-vermais')
+            opcaoPagamento.classList.remove('indicacao-modal-vermais')
         }
         if (e.target.id == 'horario') {
-            sobre.classList.remove('abrir')
-            horario.classList.add('abrir')
-            pagamento.classList.remove('abrir')
+            divSobre.classList.remove('abrir')
+            divHorario.classList.add('abrir')
+            divPagamento.classList.remove('abrir')
+
+            opcaoSobre.classList.remove('indicacao-modal-vermais')
+            opcaoHorario.classList.add('indicacao-modal-vermais')
+            opcaoPagamento.classList.remove('indicacao-modal-vermais')
         }
         if (e.target.id == 'pagamento') {
-            sobre.classList.remove('abrir')
-            horario.classList.remove('abrir')
-            pagamento.classList.add('abrir')
+            divSobre.classList.remove('abrir')
+            divHorario.classList.remove('abrir')
+            divPagamento.classList.add('abrir')
+
+            opcaoSobre.classList.remove('indicacao-modal-vermais')
+            opcaoHorario.classList.remove('indicacao-modal-vermais')
+            opcaoPagamento.classList.add('indicacao-modal-vermais')
+        }
+    })
+}
+
+function abrirOpcaoDeEntrega() {
+    const modal = document.querySelector('.janela-modal-opcaoDeEntrega')
+
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar-opcaoDeEntrega' || e.target.id == 'janela-modal-opcaoDeEntrega') {
+            modal.classList.remove('abrir')
         }
     })
 }
